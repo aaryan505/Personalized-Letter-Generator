f5f5b2ccf84722de61236cf835b88346fbe77b13
AARYAN GUPTA <126643804+aaryan505@users.noreply.github.com>
2024-08-18 13:44:38 +0530

    Add files via upload

---
+++ Mail Merge Project Start/main.py
@@ -0,0 +1,11 @@
+PLACEHOLDER = "[name]"
+with open("./input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+with open("./input/letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+       stripped_name= name.strip()
+       new_letter =  letter_contents.replace(PLACEHOLDER, stripped_name)
+       print(new_letter)
+       with open(f"./output/ReadyToSend/letter_for_{stripped_name}.docx",mode="w") as completed_letter:
+           completed_letter.write(new_letter)
